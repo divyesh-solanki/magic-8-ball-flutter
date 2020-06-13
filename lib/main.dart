56ccb71782f684b635fb6a2a67c7b4a8f13d89d6
divyesh <skdivyesh333@gmail.com>
2020-06-13 20:19:11 +0530

    added the images inside a flatbutton and imported math package for using random number generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int ballNumber = Random().nextInt(5) + 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[800],
+        ),
+        body: Center(
+          child: Container(
+            child: FlatButton(
+              child: Image.asset('images/ball$ballNumber.png'),
+              onPressed: () {
+                setState(
+                  () {
+                    ballNumber = Random().nextInt(5) + 1;
+                  },
+                );
+                print("Button clicked");
+                print(ballNumber);
+              },
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
